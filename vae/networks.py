bf728cee47b9e3ca323e8b3a974210e4bd923237
Giovani Valdrighi <41912094+GiovaniValdrighi@users.noreply.github.com>
2020-02-06 15:37:11 -0300

    added decoder class

--- vae/networks.py
+++ vae/networks.py
@@ -1,6 +1,9 @@
 import numpy as np
 import torch
-import torch.nn as nn
+from torch.nn import Module, Linear, Softplus, Sigmoid
+
+#torch.cat, torch.exp 
+#np.random.permutation
 
 def setup_data_loader(images, classes, batch_size = 128, use_CUDA = False):
   '''
@@ -26,29 +29,30 @@ def setup_data_loader(images, classes, batch_size = 128, use_CUDA = False):
   return train_loader, test_loader
 
 
-class Encoder(nn.Module):
-  '''Class that receive image and its latent value and pass trough the encoder neural network
-  with 2 layers. One layer generates the hidden variable and the second layer generate
-  the loc and scale of the z (latent space) gaussian variable.
-  
-  Input:
-  :img_dim: dimension of image vector
-  :label_dim: dimension of label vector
-  :latent_dim: dimension of latent space, output
-  
-  Output:
-  :loc_z: tensor of shape [200] with the loc of the gaussian distribution of Z variable
-  :scale_z: tensor of shape [200] with the scale of the gaussian distribution of Z variable
+class Encoder(Module):
+  '''
+    Class that receive image and its latent value and pass through the encoder neural network
+    with 2 layers. One layer generates the hidden variable and the second layer generate
+    the loc and scale of the z (latent space) gaussian variable.
+
+    Input:
+    :img_dim: dimension of image vector
+    :label_dim: dimension of label vector
+    :latent_dim: dimension of latent space, output
+
+    Output:
+    :loc_z: tensor of shape [200] with the loc of the gaussian distribution of Z variable
+    :scale_z: tensor of shape [200] with the scale of the gaussian distribution of Z variable
   '''
   def __init__(self, img_dim = 4096, label_dim = 114, latent_dim = 200):
     super(Encoder, self).__init__()
     self.img_dim = img_dim
     self.label_dim = label_dim
     self.latent_dim = latent_dim 
-    self.fc1 = nn.Linear(img_dim + label_dim, 1000)
-    self.fc21 = nn.Linear(1000, latent_dim)
-    self.fc22 = nn.Linear(1000, latent_dim)
-    self.softplus = nn.Softplus()
+    self.fc1 = Linear(img_dim + label_dim, 1000)
+    self.fc21 = Linear(1000, latent_dim)
+    self.fc22 = Linear(1000, latent_dim)
+    self.softplus = Softplus()
 
   def forward(self, img, label):
     data = torch.cat((img, label), -1)
@@ -56,3 +60,34 @@ class Encoder(nn.Module):
     loc_z = self.fc21(hidden)
     scale_z = torch.exp(self.fc22(hidden))
     return loc_z, scale_z
+ 
+
+  class Decoder(Module):
+  '''
+    Class that receive the Z variable and its latent values and pass through the decode neural network
+    with 2 layers. One layer generates the hidden variable and the second layer generate the image.
+
+    Input:
+    :img_dim: dimension of image vector
+    :label_dim: dimension of label vector
+    :latent_dim: dimension of latent space, output
+
+    Output:
+    :image: tensor of shape [4096] that is the reconstruction of the image data
+    
+  '''
+  def __init__(self, img_dim = 4096, label_dim = 114, latent_dim = 200):
+    super(Decoder, self).__init__()
+    self.img_dim = img_dim
+    self.label_dim = label_dim
+    self.latent_dim = latent_dim
+    self.fc1 = Linear(latent_dim+label_dim, 1000)
+    self.fc2 = Linear(1000, img_dim)
+    self.softplus = Softplus()
+    self.sigmoid = Sigmoid()
+
+  def forward(self, latent, label):
+    data = torch.cat((latent, label), -1)
+    hidden = self.softplus(self.fc1(data))
+    image = self.sigmoid(self.fc2(hidden))
+    return image

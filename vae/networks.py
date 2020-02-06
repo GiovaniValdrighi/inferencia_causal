07e7ac75da6a9eda37d13809b235e0004e15496a
Giovani Valdrighi <41912094+GiovaniValdrighi@users.noreply.github.com>
2020-02-06 15:29:47 -0300

    added network and encoder

--- vae/networks.py
+++ vae/networks.py
@@ -1,1 +1,58 @@
+import numpy as np
+import torch
+import torch.nn as nn
 
+def setup_data_loader(images, classes, batch_size = 128, use_CUDA = False):
+  '''
+    Function that receives two arrays, an array of the data images and an array of it latents values
+    and generate a DataLoader for train and test data.
+    
+    Input:
+    :images: array of size [*, 64, 64] with images data
+    :classes: array of size [*, 6] with latent variables of the images
+    
+    Output:
+    :train_loader: torch data loader with train data (images and latents)
+    :test_loader: torch data loader with test data (images and latents)
+  '''
+  index = np.random.permutation(imgs.shape[0])
+  images = images[index].astype(np.float32)
+  classes = classes[index].astype(np.float32)
+  train_df = torch.utils.data.TensorDataset(torch.from_numpy(images[100000:].reshape(-1, 4096)), torch.from_numpy(classes[100000:]))
+  test_df = torch.utils.data.TensorDataset(torch.from_numpy(images[:100000].reshape(-1, 4096)), torch.from_numpy(classes[:100000]))
+  kwargs = {'num_workers': 1, 'pin_memory': use_CUDA}
+  train_loader = torch.utils.data.DataLoader(train_df, batch_size, shuffle = False, **kwargs)
+  test_loader = torch.utils.data.DataLoader(test_df, batch_size, shuffle = False, **kwargs)
+  return train_loader, test_loader
+
+
+class Encoder(nn.Module):
+  '''Class that receive image and its latent value and pass trough the encoder neural network
+  with 2 layers. One layer generates the hidden variable and the second layer generate
+  the loc and scale of the z (latent space) gaussian variable.
+  
+  Input:
+  :img_dim: dimension of image vector
+  :label_dim: dimension of label vector
+  :latent_dim: dimension of latent space, output
+  
+  Output:
+  :loc_z: tensor of shape [200] with the loc of the gaussian distribution of Z variable
+  :scale_z: tensor of shape [200] with the scale of the gaussian distribution of Z variable
+  '''
+  def __init__(self, img_dim = 4096, label_dim = 114, latent_dim = 200):
+    super(Encoder, self).__init__()
+    self.img_dim = img_dim
+    self.label_dim = label_dim
+    self.latent_dim = latent_dim 
+    self.fc1 = nn.Linear(img_dim + label_dim, 1000)
+    self.fc21 = nn.Linear(1000, latent_dim)
+    self.fc22 = nn.Linear(1000, latent_dim)
+    self.softplus = nn.Softplus()
+
+  def forward(self, img, label):
+    data = torch.cat((img, label), -1)
+    hidden = self.softplus(self.fc1(data))
+    loc_z = self.fc21(hidden)
+    scale_z = torch.exp(self.fc22(hidden))
+    return loc_z, scale_z
